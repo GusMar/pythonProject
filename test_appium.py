e39f62ed1ac8163bdf23a4355b8353ec236dc7ef
gmarin <gamarin@gmail.com>
2022-01-20 15:31:36 -0500

    added passing test

--- test_appium.py
+++ test_appium.py
@@ -11,8 +11,8 @@ class AppiumTest(unittest.TestCase):
 
     def setUp(self):
         # This is the Application and ‘app’ desired capability to specify a path to Appium.
-        self.dc['app'] = '/bitrise/src/pythonProject/resources/eribank.apk'
-        # self.dc['app'] = os.environ['BITRISE_SOURCE_DIR'] + '/resources/eribank.apk'
+        # self.dc['app'] = '/bitrise/src/pythonProject/resources/eribank.apk'
+        self.dc['app'] = os.environ['BITRISE_SOURCE_DIR'] + '/resources/eribank.apk'
         # self.dc['app'] = os.path.join( str(Path(__file__).parent),'resources', 'eribank.apk')
         # self.dc['app'] = os.path.join(str(Path(__file__).parent), 'resources', 'eribank.apk')
         # "C:\\Users\\gmarin\\PycharmProjects\\FirstPythonAppiumTest\\resources\\eribank.apk"
